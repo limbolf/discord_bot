85c0917458755e3a7625d3e558166e1a265ce2fd
limbolf <vincentdecoopman0@gmail.com>
2026-06-26 16:14:09 +0200

    adding firsts commands

---
+++ commands/utility/user.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { execute } = require("./ping");
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("user")
+    .setDescription("Provides user Information"),
+  async execute(interaction) {
+    await interaction.reply(
+      "This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}. ",
+    );
+  },
+};
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // * Prérequis d'importations
-
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+const {
+  Client,
+  Events,
+  GatewayIntentBits,
+  resolveBuilder,
+  Collection,
+} = require("discord.js");
 const { BOT_TOKEN } = require("./.env");
 require("dotenv").config();
 // * On créer la nouvelle instance du bot
@@ -11,3 +18,52 @@ client.once(Events.ClientReady, (readyClient) => {
 });
 
 client.login(process.env.BOT_TOKEN);
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        '[WARNING] The comand at ${filePath} is missing a required "data" of "execute" property.',
+      );
+    }
+  }
+}
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error("No command matching ${interaction.commandName} was found");
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "An error occured while executing the command !",
+        flags: MessageFlags.Ephemeral,
+      });
+    } else {
+      await interaction.reply({
+        content: "An error occured while executing the command !",
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+  }
+  console.log(interaction);
+});
