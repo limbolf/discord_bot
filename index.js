ac071772bc0b7c6ebf1e19b5f1cca7c77e03089d
limbolf <vincentdecoopman0@gmail.com>
2026-07-18 21:55:27 +0200

    adjusting typing errors

--- index.js
+++ index.js
@@ -32,10 +32,9 @@ for (const folder of commandFolders) {
 }
 
 const eventsPath = path.join(__dirname, "events");
-const eventsFiles = fs
+const eventFiles = fs
   .readdirSync(eventsPath)
   .filter((file) => file.endsWith(".js"));
-
 for (const file of eventFiles) {
   const filePath = path.join(eventsPath, file);
   const event = require(filePath);
